89181074e80bdb3846057c1645089f679f022d53
Alexey <example>
2019-07-03 13:54:07 +0300

    Бухгалтерия

---
+++ js.js
@@ -0,0 +1,28 @@
+if (2*4 == 7) {
+    console.log('Верно')
+} else {
+    console.log("Неверно")
+}
+
+
+
+let num = 50;
+// while (num < 155 ) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++; 
+}
+while (num < 55);
+
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue 
+    }
+          
+    console.log(i);
+}
--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let money = prompt('Ваш бюджет на месяц?', ''),
-time = prompt('Введите дату  в формате YYYY-MM-DD', ''),
-trata1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-price1 = prompt('Восколько обойдется?', ''),
-trata2 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-price2 = prompt('Восколько обойдется?', '');
-
+let money = +prompt('Ваш бюджет на месяц?', ''),
+time = prompt('Введите дату  в формате YYYY-MM-DD', '');
 
 let appData = {
     budget: money,
@@ -15,8 +10,31 @@ let appData = {
     expenses: {}
 };
 
-appData.expenses.trata1 = price1;
-appData.expenses.trata2 = price2;
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Восколько обойдется?', '');
+if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a!= '' && b!= '' && a.length < 50)  
+{
+    console.log('Done');
+    appData.expenses[a] = b;
+}
+else {
 
-alert("Ваш бюджет на день: "+ appData.budget/30);
+}
+};
+
+appData.moneyPerDay = appData.budget/30;
+
+
+alert("Ваш бюджет на день: "+ appData.moneyPerDay );
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка")
+}
 
