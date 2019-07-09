f1797dff23e1ebf6f60fcda7e30dfe1fcc0e23c1
aleksey <afilatov2002@gmail.com>
2019-07-09 22:53:55 +0300

    semicolons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let money, time
+let money, time;
 
 function detectDayBudget() {
    money = +prompt('Ваш бюджет на месяц?', ''); 
@@ -11,8 +11,8 @@ detectDayBudget();
 
 
 function start() {
-    time = +prompt('Введите дату  в формате YYYY-MM-DD', '')
-};
+    time = +prompt('Введите дату  в формате YYYY-MM-DD', '');
+}
 
 start();
 
@@ -33,7 +33,7 @@ if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a!
     appData.expenses[a] = b;
 }
 else {
-       i = i -1 }
+       i = i - 1; }
     } 
     }
 };
@@ -48,13 +48,13 @@ appData.moneyPerDay = (appData.budget/30).toFixed();
 
 function detectLevel() {
  if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка")
+    console.log("Минимальный уровень достатка");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
     console.log("Средний уровень достатка");
 } else if (appData.moneyPerDay > 2000) {
     console.log("Высокий уровень достатка");
 } else {
-    console.log("Произошла ошибка")
+    console.log("Произошла ошибка");
 }   
 }
 
@@ -79,6 +79,6 @@ for (i = 1 ; i <4; i++ ) {
    appData.optionalExpenses[i] = optionExpense;
 }
  
-};
+}
 
 chooseOptExpenses();
